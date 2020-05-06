dd66fde349597fa0c0c65757bf8bebb9e1a65f14
Christine Ayoh <christineayoh9@gmail.com>
2020-05-06 21:10:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,25 +16,12 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-
  		WIN_COMBINATIONS.each do |combo|
-		index1 = combo[0]
-		index2 = combo[1]
-		index3 = combo[2]
-		position_1 = board[index1] # load the value of the board at win_index_1
-	 	position_2 = board[index2] # load the value of the board at win_index_2
-	 	position_3 = board[index3] # load the value of the board at win_index_3
-
-	 	if position_1 == "X" && position_2 == "X" && position_3 == "X"
-	   		return combo # return the win_combination indexes that won.
-		else
-			nil
-	  	end
-		if position_1 == "O" && position_2 == "O" && position_3 == "O"
-			return combo # return the win_combination indexes that won.
-		else
-			nil
-		end
+			if board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]]
+	   			return combo # return the win_combination indexes that won.
+			else
+				nil
+	  		end
 	end
 end
 
@@ -48,30 +35,14 @@ def winner(board)
 end
 
 def over?(board)
-	if won?(board)
-		true
-	elsif draw?(board)
-		true
-	elsif full?(board)
-		true
-	else
-		false
-	end
+	won?(board) ||  draw?(board) || full?(board)
+
 end
 
 def draw?(board)
-	if full?(board)
-		if !won?(board)
-			true
-		end
-	else
-		false
-	end
+	full?(board) && !won?(board)
 end
 
 def full?(board)
-	if board.any?{ |index| index != "X" || index != "O" }
-		false
-	true
-	end
+	board.all?{ |index| index == "X" || index == "O" }
 end
